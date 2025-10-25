aaa11800285b3179d090ca7cadc969f1fb827cce
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2025-10-25 20:40:10 +0530

    partice previous day code to recall

--- src/components/Partice.js
+++ src/components/Partice.js
@@ -1,10 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 const Partice = () => {
     const [count, setCount] = useState(0)
     const [hide, Unhide] = useState(true)
     const [text, setText] = useState('')
+    const [colorCode, setColorCode] = useState('')
+    const [chCount, setChCount] = useState('')
+    const [dButton, setDButton] = useState('')
+
+
+
+    const randomClr = () => {
+        let clrCode = '0123456789ABCDEF'
+        let result = '#'
+        for (let i = 0; i < 6; i++) {
+            let randomIndex = Math.floor(Math.random() * clrCode.length)
+            result += clrCode[randomIndex]
+        }
+        setColorCode(result)
+    }
+    useEffect(() => {
+        randomClr()
+        let intervalID = setInterval(() => {
+            randomClr()
+        }, 1000)
+        return () => clearInterval(intervalID)
+    }, [])
 
 
 
@@ -19,9 +41,17 @@ const Partice = () => {
 
     }
 
+
     const textChange = (e) => {
         setText(e.target.value)
     }
+
+    const textType = (e) => {
+        setChCount(e.target.value)
+    }
+
+
+
     return (
 
         <div>
@@ -49,7 +79,39 @@ const Partice = () => {
                 <p>{text}</p>
             </div>
 
+            <div style={{ backgroundColor: colorCode }}>
+                <h1>{colorCode}</h1>
+                <button onClick={randomClr}>Generate color</button>
+            </div>
+            /ch Counter
+            <div>
+                <input type="text"
+                    value={chCount}
+                    placeholder='type any character'
+                    onChange={(e) => textType(e)}
+
+
+
+                />
+                <h1>text:-{chCount}---length is -{chCount.length}</h1>
+            </div>
+
+
+            /diasabl button
+            <div >
+                <input type="text"
+                    placeholder='type ur name'
+                    value={dButton}
+                    onChange={(e) => setDButton(e.target.value)}
+
+                />
+
+                {dButton.length > 0 && (<button>submit</button>)}
+
+
+            </div>
         </div>
+
     )
 }
 
